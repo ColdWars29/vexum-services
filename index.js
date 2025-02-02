8030890ffed08869ae369a2cc3fa1e5db4d9ba3e
ColdWars29 <29GariglioSe@fortcherry.org>
2025-02-01 21:32:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = 8080;
 
-app.use(express.static(path.join(__dirname, 'index.html')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
