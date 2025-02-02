aab0b0da0b10e9ed6b2d5412212eab8c0f6ebc22
ColdWars29 <29GariglioSe@fortcherry.org>
2025-02-01 21:22:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 8080;
 
-// Basic endpoint
-app.get('/', (req, res) => {
-    app.use(express.static(path.join(__dirname, 'index.html')));
-});
-
-// Health check endpoint
-app.get('/health', (req, res) => {
-    res.status(200).send('Healthy');
-});
-
-// Start the server
+app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
